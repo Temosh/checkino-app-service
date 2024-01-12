d6750b7924ad2b88db954cc5fe3b3d630129acbb
Serhii Tymoshenko <temosh@i.ua>
2024-01-12 20:11:11 +0800

    Initial project template

---
+++ src/main/java/com/summ/checkino/app/CheckinoAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.summ.checkino.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinoAppServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckinoAppServiceApplication.class, args);
+    }
+
+}
